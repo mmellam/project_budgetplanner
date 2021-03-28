bde4ff5809a96d80a73f9d292397edd0c6c232d1
mmellam <28260088+mmellam@users.noreply.github.com>
2021-03-28 13:19:46 +0200

    Implemented Add, Subtract, Transfer and DELETE route handlers

--- app.js
+++ app.js
@@ -87,9 +87,55 @@ envRouter.put('/:envelopeId', (req, res) => {
 
 envRouter.post('/:envelopeId/add', (req, res) => {
     const envelopeId = req.params.envelopeId;
+    const toAdd = Number(req.query.amount);
+    if (!account[0][envelopeId]) {
+        res.status(404).send(`No envelope with ID of ${envelopeId}`);
+    } else if (isNaN(toAdd)) {
+        res.status(400).send(`Please enter a valid number as amount`)
+    } else {
+        account[0][envelopeId].budget += toAdd;
+        res.status(200).send(`Envelope with ID of ${envelopeId} and title ${account[0][envelopeId].title}: after adding ${toAdd}, the new budget is ${account[0][envelopeId].budget}`);
+    }
 })
 envRouter.post('/:envelopeId/subtract', (req, res) => {
     const envelopeId = req.params.envelopeId;
+    const toSubtract = Number(req.query.amount);
+
+    if (!account[0][envelopeId]) {
+        res.status(404).send(`No envelope with ID of ${envelopeId}`);
+    } else if (isNaN(toSubtract)) {
+        res.status(400).send(`Please enter a valid number as amount`)
+    } else {
+        account[0][envelopeId].budget -= toSubtract;
+        res.status(200).send(`Envelope with ID of ${envelopeId} and title ${account[0][envelopeId].title}: after subtracting ${toSubtract}, the new budget is ${account[0][envelopeId].budget}`);
+    }
+})
+
+envRouter.post('/transfer', (req, res) => {
+    const from = req.query.from;
+    const to = req.query.to;
+    const amount = Number(req.query.amount);
+
+    if (!account[0][from] || !account[0][to] || isNaN(amount)) {
+        res.status(400).send(`Please enter valid IDs and/or valid amount`)
+    } else {
+        account[0][from].budget -= amount;
+        account[0][to].budget += amount;
+
+        res.send(`Updated budget in env ${from}: ${account[0][from].budget}\n
+        Updated budget in env ${to}: ${account[0][to].budget}`)
+    }
+})
+
+envRouter.delete('/:envelopeId', (req, res) => {
+    const envelopeId = req.params.envelopeId;
+
+    if (!account[0][envelopeId]) {
+        res.status(400).send(`An envelope with ID ${envelopeId} was not found`);
+    } else {
+        delete account[0][envelopeId];
+        res.send(`Envelope with ID ${envelopeId} was successfully deleted`);
+    }
 })
 
 app.listen(PORT, () => {
