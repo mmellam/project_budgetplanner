2d09be9340e79c4e0999ce113407a3b7a2067a73
mmellam <28260088+mmellam@users.noreply.github.com>
2021-03-28 14:20:00 +0200

    Refactored with .param() to attach id

--- app.js
+++ app.js
@@ -33,81 +33,81 @@ app.post('/test', (req, res) => {
 const envRouter = express.Router();
 app.use('/envelopes', envRouter);
 
+envRouter.param('envelopeId', (req, res, next, id) => {
+    const envelopeId = id;
+    try {
+        if (account[0][envelopeId]) {
+            req.envelopeId = envelopeId;
+            next();
+        } else {
+            return res.status(404).send(`An envelope with ID ${envelopeId} was not found`);
+        }
+    } catch(err) {
+        next(err);
+    }
+})
+
 envRouter.get('/', (req, res) => {
     res.json(account)
 })
 envRouter.get('/:envelopeId', (req, res) => {
-    const envelopeId = req.params.envelopeId;
-    if (account[0][envelopeId]) {   
-        res.status(200).send(`Envelope with ID ${envelopeId} has the title ${account[0][envelopeId].title} and a budget of ${account[0][envelopeId].budget}`);
-        
+    res.status(200).send(`Envelope with ID ${req.envelopeId} has the title ${account[0][req.envelopeId].title} and a budget of ${account[0][req.envelopeId].budget}`);
+})
+envRouter.put('/:envelopeId', (req, res) => {
+    const budget = req.query.budget;
+    const title = req.query.title;
+    
+    if (budget && title) {
+        account[0][req.envelopeId].budget = budget;
+        account[0][req.envelopeId].title = title;
+        res.status(200).send(`Envelope with ID ${req.envelopeId} was updated with title ${account[0][req.envelopeId].title} and updated with a budget of ${account[0][req.envelopeId].budget}`);
+    } else if (!title) {
+        account[0][req.envelopeId].budget = budget;
+        res.status(200).send(`Envelope with ID ${req.envelopeId} and title ${account[0][req.envelopeId].title} was updated with a budget of ${account[0][req.envelopeId].budget}`);
     } else {
-        res.status(404).send(`No envelope with ID of ${envelopeId}`);
-    }
+        account[0][req.envelopeId].title = title;
+        res.status(200).send(`Envelope with ID ${req.envelopeId} was updated with title ${account[0][req.envelopeId].title} and has a budget of ${account[0][req.envelopeId].budget}`);
+    }     
+})
+envRouter.delete('/:envelopeId', (req, res) => {
+    delete account[0][req.envelopeId];
+    res.send(`Envelope with ID ${req.envelopeId} was successfully deleted`);
 })
 
-envRouter.post('/', (req, res) => {
-    const envelopeId = req.query.envelopeId;
+envRouter.post('/new', (req, res) => {
+    const id = req.query.id;
     const title = req.query.title;
     const budget = req.query.budget;
-    if (!account[0][envelopeId]) {
-        account[0][envelopeId] = {
+    if (!account[0][id]) {
+        account[0][id] = {
             title: title,
             budget: budget
         }
-        res.status(201).send(`New envelope created: Id ${envelopeId}, title ${account[0][envelopeId].title}, budget ${account[0][envelopeId].budget}`);
+        res.status(201).send(`New envelope created: Id ${id}, title ${account[0][id].title}, budget ${account[0][id].budget}`);
     } else {
-        res.send(`This envelope with ID ${envelopeId} already exists. Content: title ${account[0][envelopeId].title}, budget ${account[0][envelopeId].budget}.
+        res.send(`This envelope with ID ${id} already exists. Content: title ${account[0][id].title}, budget ${account[0][id].budget}.
         To update its budget, please send a put request.
         To create a new envelope, increase the id number.`)
     }
 })
 
-envRouter.put('/:envelopeId', (req, res) => {
-    const envelopeId = req.params.envelopeId;
-    const budget = req.query.budget;
-    const title = req.query.title;
-    
-    if (account[0][envelopeId]) { 
-        if (budget && title) {
-            account[0][envelopeId].budget = budget;
-            account[0][envelopeId].title = title;
-            res.status(200).send(`Envelope with ID ${envelopeId} was updated with title ${account[0][envelopeId].title} and updated with a budget of ${account[0][envelopeId].budget}`);
-        } else if (!title) {
-            account[0][envelopeId].budget = budget;
-            res.status(200).send(`Envelope with ID ${envelopeId} and title ${account[0][envelopeId].title} was updated with a budget of ${account[0][envelopeId].budget}`);
-        } else {
-            account[0][envelopeId].title = title;
-            res.status(200).send(`Envelope with ID ${envelopeId} was updated with title ${account[0][envelopeId].title} and has a budget of ${account[0][envelopeId].budget}`);
-        }     
-    } else {
-        res.status(404).send(`No envelope with ID of ${envelopeId}`);
-    }
-})
-
 envRouter.post('/:envelopeId/add', (req, res) => {
-    const envelopeId = req.params.envelopeId;
     const toAdd = Number(req.query.amount);
-    if (!account[0][envelopeId]) {
-        res.status(404).send(`No envelope with ID of ${envelopeId}`);
-    } else if (isNaN(toAdd)) {
+    if (isNaN(toAdd)) {
         res.status(400).send(`Please enter a valid number as amount`)
     } else {
-        account[0][envelopeId].budget += toAdd;
-        res.status(200).send(`Envelope with ID of ${envelopeId} and title ${account[0][envelopeId].title}: after adding ${toAdd}, the new budget is ${account[0][envelopeId].budget}`);
+        account[0][req.envelopeId].budget += toAdd;
+        res.status(200).send(`Envelope with ID of ${req.envelopeId} and title ${account[0][req.envelopeId].title}: after adding ${toAdd}, the new budget is ${account[0][req.envelopeId].budget}`);
     }
 })
 envRouter.post('/:envelopeId/subtract', (req, res) => {
-    const envelopeId = req.params.envelopeId;
     const toSubtract = Number(req.query.amount);
 
-    if (!account[0][envelopeId]) {
-        res.status(404).send(`No envelope with ID of ${envelopeId}`);
-    } else if (isNaN(toSubtract)) {
+    if (isNaN(toSubtract)) {
         res.status(400).send(`Please enter a valid number as amount`)
     } else {
-        account[0][envelopeId].budget -= toSubtract;
-        res.status(200).send(`Envelope with ID of ${envelopeId} and title ${account[0][envelopeId].title}: after subtracting ${toSubtract}, the new budget is ${account[0][envelopeId].budget}`);
+        account[0][req.envelopeId].budget -= toSubtract;
+        res.status(200).send(`Envelope with ID of ${req.envelopeId} and title ${account[0][req.envelopeId].title}: after subtracting ${toSubtract}, the new budget is ${account[0][req.envelopeId].budget}`);
     }
 })
 
@@ -124,17 +124,6 @@ envRouter.post('/transfer', (req, res) => {
 
         res.send(`Updated budget in env ${from}: ${account[0][from].budget}\n
         Updated budget in env ${to}: ${account[0][to].budget}`)
-    }
-})
-
-envRouter.delete('/:envelopeId', (req, res) => {
-    const envelopeId = req.params.envelopeId;
-
-    if (!account[0][envelopeId]) {
-        res.status(400).send(`An envelope with ID ${envelopeId} was not found`);
-    } else {
-        delete account[0][envelopeId];
-        res.send(`Envelope with ID ${envelopeId} was successfully deleted`);
     }
 })
 
